002a0e4e0bbcc6728d52c3ac6b49ba5bcc632683
Anton.Forbes <Anton.Forbes1@jet2.com>
2019-01-18 13:47:05 +0000

    Halfway through 1.3

---
+++ src/Unit-1/DoThis/ValidatorActor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace WinTail
+{
+    class ValidatorActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+        public ValidatorActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as String;
+            if (String.IsNullOrEmpty(msg))
+            {
+                _consoleWriterActor.Tell(new Messages.NullInputError("No input received."));
+            }
+            else
+            {
+                if(IsValid(msg))
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you, valid message"));
+                }
+                else
+                {
+                    _consoleWriterActor.Tell(new Messages.InputError("Invalid: odd number of chars"));
+                }
+            }
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private bool IsValid(string msg)
+        {
+            return msg.Length % 2 == 0;
+        }
+    }
+}

4695dd3846c1f98b7578674fff0b6414813b22a0
Anton.Forbes <Anton.Forbes1@jet2.com>
2019-01-22 08:52:14 +0000

    halfway through 2.3.6

---
+++ src/Unit-2/DoThis/Actors/ButtonToggleActor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    class ButtonToggleActor : UntypedActor
+    {
+        #region Message Types
+        public class Toggle { }
+
+#endregion
+
+        private readonly CounterType _myCounterType;
+        private bool _isToggledOn;
+        private readonly Button _myButton;
+        private readonly IActorRef _coordinatorActor;
+
+        public ButtonToggleActor(IActorRef coordinatorActor, Button myButton, CounterType myCounterType, bool isToggledOn = false)
+        {
+            _coordinatorActor = coordinatorActor;
+            _myButton = myButton;
+            _myCounterType = myCounterType;
+            _isToggledOn = isToggledOn;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Toggle && !_isToggledOn)
+            {
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Unwatch(_myCounterType));
+                FlipToggle();
+            }
+            else if (message is Toggle && !_isToggledOn)
+            {
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Watch(_myCounterType));
+                FlipToggle();
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+
+        private void FlipToggle()
+        {
+            _isToggledOn = !_isToggledOn;
+            _myButton.Text = $"{_myCounterType.ToString().ToUpperInvariant()} ({(_isToggledOn ? "ON" : "OFF")})";
+        }
+    }
+}
